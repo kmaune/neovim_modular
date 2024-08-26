f11fee920b461b80340b3cc7cfa2a0616d86d57d
kmaune@umich.edu <kmaune@umich.edu>
2024-08-25 20:28:48 -0400

    fix merge mistake

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -170,7 +170,7 @@ return {
       local servers = {
         clangd = { cmd = {"clangd", "--offset-encoding=utf-16"}, },
         -- gopls = {},
-        pyright = {},
+        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
