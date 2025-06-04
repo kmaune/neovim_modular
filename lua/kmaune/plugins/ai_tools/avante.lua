b2617292a8362ae00f613d8d0f82bd39bfdf034f
kmaune <kmaune@umich.edu>
2025-06-04 01:32:55 -0400

    avante config update after breaking plugin update

--- lua/kmaune/plugins/ai_tools/avante.lua
+++ lua/kmaune/plugins/ai_tools/avante.lua
@@ -7,8 +7,10 @@ return {
       -- add any opts here
       -- for example
       provider = 'ollama',
-      ollama = {
-        model = 'qwen2.5-coder:14b',
+      providers = {
+        ollama = {
+          model = 'qwen2.5-coder:14b',
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
