31bbbe658b0b23defaecaff430e9a56b1a5c5288
kmaune <kmaune@umich.edu>
2025-06-17 20:08:56 -0400

    nvim-cmp, and use non-symlink path to fix obsidian.nvim

--- lua/kmaune/plugins/obsidian.lua
+++ lua/kmaune/plugins/obsidian.lua
@@ -5,47 +5,79 @@ return {
   ft = 'markdown',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    -- Add nvim-cmp specifically for obsidian
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
   },
-  opts = {
-    workspaces = {
-      {
-        name = 'personal',
-        path = '~/Documents/ObsidianVault-Main',
-      },
-    },
+  config = function()
+    -- Set up nvim-cmp specifically for markdown files (obsidian)
+    local cmp = require 'cmp'
 
-    daily_notes = {
-      folder = '00 - Daily/Daily Notes',
-      date_format = '%Y-%m-%d',
-    },
+    -- Configure cmp only for markdown files
+    cmp.setup.filetype('markdown', {
+      sources = cmp.config.sources {
+        { name = 'obsidian' },
+        { name = 'obsidian_new' },
+        { name = 'obsidian_tags' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
+      mapping = cmp.mapping.preset.insert {
+        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+    })
 
-    -- Enable completions
-    completion = {
-      --nvim_cmp = true,
-      blink = true,
-      min_chars = 2,
-    },
+    -- Set up obsidian with the real path (not symlink)
+    require('obsidian').setup {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '/Users/kmaune/Library/Mobile Documents/iCloud~md~obsidian/Documents/ObsidianVault-Main',
+        },
+      },
 
-    -- Default keymaps (explicitly defined)
-    mappings = {
-      ['gf'] = {
-        action = function()
-          return require('obsidian').util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
+      daily_notes = {
+        folder = '00 - Daily/Daily Notes',
+        date_format = '%Y-%m-%d',
       },
-      ['<leader>ch'] = {
-        action = function()
-          return require('obsidian').util.toggle_checkbox()
-        end,
-        opts = { buffer = true },
+
+      -- Enable completions with nvim-cmp
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
       },
-      ['<cr>'] = {
-        action = function()
-          return require('obsidian').util.smart_action()
-        end,
-        opts = { buffer = true, expr = true },
+
+      -- Keymaps for following links and other obsidian actions
+      mappings = {
+        ['gf'] = {
+          action = function()
+            return require('obsidian').util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ['<leader>ch'] = {
+          action = function()
+            return require('obsidian').util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        ['<cr>'] = {
+          action = function()
+            return require('obsidian').util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
       },
-    },
-  },
+    }
+  end,
 }
